ab063e24d4b4b39ae1b4a443837abc79c4a7369b
nevim <ondrej.lupisek@gmail.com>
2026-05-12 11:24:56 +0200

    inital commit

---
+++ autolock.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+#!/bin/sh
+
+exec xautolock -detectsleep \
+	-locker "i3lock-fancy -d -c 000070" \
---
+++ theme.lua
@@ -0,0 +1,4 @@
+local theme = dofile("/usr/share/awesome/themes/default/theme.lua")
+theme.wallpaper_cmd = { }
+theme.wallpaper = "/home/nevim/Pictures/endeavourOS gemini.png"
+return theme
