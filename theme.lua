7802bf506d5411555382ee22362af224e2d638d2
nevim <ondrej.lupisek@gmail.com>
2026-06-29 01:36:31 +0200

    added wallpapers and screen ration - wallpaper matching

--- theme.lua
+++ theme.lua
@@ -1,4 +1,12 @@
 local theme = dofile("/usr/share/awesome/themes/default/theme.lua")
 theme.wallpaper_cmd = { }
-theme.wallpaper = "/home/nevim/Pictures/endeavourOS gemini.png"
+theme.wallpaper = function(s)
+	if s then
+		if s.geometry.width / s.geometry.height > 1.5 then
+			return "/home/nevim/.config/awesome/wallpaper" -- this is the wallpaper with picture
+		else
+			return "/home/nevim/.config/awesome/plain-wallpaper" -- this is without
+		end
+	end
+end
 return theme
